8f9029cd76e3ff0fab30f1b34446f1867080d3ce
Shenoy <adshenoy@informatica.com>
2021-06-12 19:27:44 +0530

    edited About text part

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -43,11 +43,13 @@ export const About = ()=>{
                         I am Aditya Shenoy, currently working as a R&D QA Engineer at Informatica.
                         <br/>I am passionate about programming and strive to write structured code, always  interested in designing android application. Currently trying to learn Front-end development with React-Js.
                         <br/>I have experience of working on Informatica Power Center, IDQ and IICS  and Microsoft Dynamics CRM 
-                        <br/>As a QA Engineer I have gained ability to think out of the box and identify the issue before hand.
                         <br/>My favorite language is JAVA (I love structured code),  C++,  React JS(just started and already loving it).
+                        <br/>3 start on codechef and active on stackoverflow
+                        <br/><span style={{color:'#FF9F1C', fontStyle:'normal'}}>Technical Skills other than programming languages:-</span> 
+                        <br/>Data structures, Linux
                         <br/><span style={{color:'#FF9F1C', fontStyle:'normal'}}>Fun facts:-</span>
-                        <br/>I am hardcore FRIENDS fan
                         <br/>I get my dopamine shots on board of 64 squares
+                        <br/>I am FRIENDS fan
                         </p>
                             
                     </motion.div>   
