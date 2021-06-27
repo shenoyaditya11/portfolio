438d98bde37b743c1a5b72c7caa5edd5e6ee5dd7
Shenoy <shenoyaditya11@gmail.com>
2021-06-27 09:48:33 +0530

    Contact page reduced extra space

--- src/Components/Frontface.jsx
+++ src/Components/Frontface.jsx
@@ -39,7 +39,7 @@ export const BackFace =(props)=>{
                     Github:- 
                 </td>
                 <td>
-                   <i class="card-icon fab fa-github" aria-hidden="true" onClick={()=>openNewWindow("https://github.com/shenoyaditya11")} > </i>
+                   <i class="card-icon fab fa-github" aria-hidden="true" onClick={()=>openNewWindow(props.project.git, event)} > </i>
                 </td>
             
             </tr>
